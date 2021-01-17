cf1da77a55d0cb1ae66d610b6fb7eadc3f24e65b
ablacklama <ablacklama@gmail.com>
2021-01-17 14:02:16 -0800

    :construction_worker: initial setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sql2sheets", # Replace with your own username
+    version="0.0.1",
+    author="Spencer Kelly",
+    author_email="spencerwellskelly@gmail.com",
+    description="Connect and schedule sql queries to be dumped in google sheets.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ablacklama/sql2sheets",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
